7c3d67da3effb8e1c7c521451397c03652c2d4c1
Pratham <pratham84540@gmail.com>
2024-05-25 10:13:03 +0530

    hey

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://pratham84540:K8O02J4IstVUL99u@cluster0.5yrnw5f.mongodb.net/"
-);
+mongoose.connect(process.env.mongo_url);
 const todoSchema = mogoose.Schema({
   title: String,
   description: String,
--- Backend/index.js
+++ Backend/index.js
@@ -2,31 +2,34 @@ const express = require("express");
 import { createTodo } from "./types";
 const app = express();
 app.use(express.json());
-app.post("/todo",function(req,res){
-    const createpayload = req.body;
-    const parsepayload = createTodo.safeParse(createpayload);
-    if(!parsepayload.sucess){
-        res.status(411).json({
-            msg:"You sent the wrong inputs",
-        })
-        return;
-    }
-
-
-
-})
-app.get("/todos",(req,res){
-
-})
-app.put("/completed", function(req,res){
-    const updatePayload  = req.body;
-    const parsePayload = updateTodo.safeParse(updatePayload);
-    if(!parsepayload.sucess){
-        res.status(411).json({
-            msg:"You sent the wrong inputs",
-        })
-        return;
-    }
-
-
-})
+const { todo } = require("./db");
+app.post("/todo", async function (req, res) {
+  const createpayload = req.body;
+  const parsepayload = createTodo.safeParse(createpayload);
+  if (!parsepayload.sucess) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+  await todo.create({
+    title: createpayload.title,
+    description: createpayload.description,
+  });
+  res.json({
+    message: "Todo created",
+  });
+});
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find();
+});
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsePayload = updateTodo.safeParse(updatePayload);
+  if (!parsepayload.sucess) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+});
